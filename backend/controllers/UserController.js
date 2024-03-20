2d92156b3e67bbb50a7a66df03e47b76eaf07e65
joren97x <sumagangjoren@gmail.com>
2024-03-20 09:13:26 +0000

    im doing prototyping dees days

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,9 +1,10 @@
 const { User } = require('../models')
 
 const index = async (req, res) => {
+    const {user_id} = req.params.id
     try {
         const users = await User.findAll()
-        return res.json({users})
+        return res.json({ users })
     }
     catch(err) {
         return res.status(500).json({ message: "Server error" })
